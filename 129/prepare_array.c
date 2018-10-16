76b6b44d42c50b18cc64f5739e820dc205c78f01
Robert Chen <robert.chen.kfc@gmail.com>
2018-10-16 14:15:17 +0800

    Solved 129

---
+++ 129/prepare_array.c
@@ -0,0 +1,10 @@
+#include "prepare_array.h"
+
+void prepare_array(int buffer[], int *array[], int row, int column[]) {
+  int *ptr = buffer;
+  for (int i = 0; i < row; ++i) {
+    array[i] = ptr;
+    ptr += column[i];
+  }
+}
+
