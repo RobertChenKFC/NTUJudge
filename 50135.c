44a35b632ee22596c1d715403922edd27f08a70e
Robert Chen <robert.chen.kfc@gmail.com>
2018-11-21 21:39:31 +0800

    Added new solution to 50135

--- 50135.c
+++ 50135.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
+void Swap(char *a, char *b) {
+  char t = *a;
+  *a = *b;
+  *b = t;
+}
+
+void Flip(char *s) {
+  Swap(s, s + 7);
+  Swap(s + 1, s + 6);
+  Swap(s + 2, s + 5);
+  Swap(s + 3, s + 4);
+}
+
 int main() {
   int type;
   scanf("%d", &type);
+  char buf[100005] = {0}; 
+  char *s = buf;
   if (type == 0) {
-    char s[100005]; 
     scanf("%s", s);
-    unsigned long long x = 0;
-    int len = strlen(s);
-    int shift = 56;
-    for (int i = 0; i <= len; ++i) {
-      if (shift == -8) {
-        printf("%llu\n", x); 
-        x = 0;
-        shift = 56;
-      } 
-      x |= (unsigned long long)s[i] << shift;
-      shift -= 8;
+    int cur = 0, len = strlen(s);
+    while (cur <= len) {
+      Flip(s);
+      printf("%llu\n", *((unsigned long long*)s));
+      s += 8;
+      cur += 8;
     }
-    if (shift != 56) printf("%llu\n", x);
   } else if (type == 1) {
     int n;
     scanf("%d", &n);
     while (n--) {
-      unsigned long long x;
-      scanf("%llu", &x);
-      for (int shift = 56; shift != -8; shift -= 8) {
-        char c = x >> shift;
-        if (c == '\0') {
-          putchar('\n');
-          break;
-        }
-        putchar(c);
-      }
+      scanf("%llu", (unsigned long long*)s);
+      Flip(s);
+      s += 8;
     }
+    printf("%s\n", buf);
   }
 }
 
